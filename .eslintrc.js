c309855256b0cb3c9da4a75a1056c19f1363c61b
Miliky <li1063094734@163.com>
2022-04-06 11:16:12 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+/** @format */
+
+module.exports = {
+	printWidth: 120,
+	// 在句尾添加分号
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	// 在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	// 为单行箭头函数的参数添加圆括号。
+	arrowParens: 'avoid',
+	insertPragma: false,
+	tabWidth: 2,
+	useTabs: true,
+	// 行结束
+	endOfLine: 'lt',
+};
