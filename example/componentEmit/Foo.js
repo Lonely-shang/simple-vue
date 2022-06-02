766b9f581f6f8e36abada1bc53f5bc50eaf1cc02
Miliky <li1063094734@163.com>
2022-06-02 14:52:32 +0800

    emit init

--- example/componentEmit/Foo.js
+++ example/componentEmit/Foo.js
@@ -2,9 +2,21 @@
 export const Foo = {
   name: 'Foo',
   setup (props) {
+
+    const emitClick = () => {
+      console.log('click');
+    }
+
+    return {
+      emitClick
+    }
+
   },
 
   render (h) {
-    return h('div', {}, 'Hello ')
+    const Btn = h('button', {
+      onClick: this.emitClick
+    }, 'emit click')
+    return h('div', {}, [Btn])
   }
 }
