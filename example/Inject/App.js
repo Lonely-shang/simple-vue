df2bf6f3636031c2f3734e7ea582ac66200f2ed2
Miliky <li1063094734@163.com>
2022-06-22 17:02:45 +0800

    init provide/inject api

---
+++ example/Inject/App.js
@@ -0,0 +1,33 @@
+
+export const  App = {
+  name: 'App',
+  setup() {
+    provide('foo', 'fooVal');
+    provide('bar', 'barVal');
+  },
+  render(h) {
+    return h(
+      'div',
+      {},
+      [
+        h('div', {}, 'Provide'),
+        h(Cusumer)
+      ]
+    )
+  }
+}
+
+const Cusumer = {
+  name: 'Inject',
+  setup() {
+    const foo = inject('foo');
+    const bar = inject('bar');
+    return {
+      foo,
+      bar
+    }
+  },
+  render(h) {
+    return h('div', {}, `Cusumer - ${this.foo} - ${this.bar}`)
+  }
+}
