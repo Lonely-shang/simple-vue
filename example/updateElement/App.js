64d4796db4b14e83ff289c8cb7f2459454f3e204
miliky <li1063094734@163.com>
2022-07-18 20:22:01 +0800

    feat: init update props

--- example/updateElement/App.js
+++ example/updateElement/App.js
@@ -7,11 +7,34 @@ export const App = {
 
     const onClick = () => {
       count.value ++
+    };
+
+    const props = ref({
+      foo: 'foo',
+      bar: 'bar'
+    })
+
+    const onChangePropsDemo1 = () => {
+      props.value.foo = 'newFoo'
+    }
+
+    const onChangePropsDemo2 = () => {
+      props.value.foo = undefined
+    }
+
+    const onChangePropsDemo3 = () => {
+      props.value = {
+        foo: 'foo'
+      }
     }
 
     return {
       count,
-      onClick
+      props,
+      onClick,
+      onChangePropsDemo1,
+      onChangePropsDemo2,
+      onChangePropsDemo3
     }
   },
 
@@ -19,11 +42,15 @@ export const App = {
     return h(
       'div',
       {
-        id: 'root'
+        id: 'root',
+        ...this.props
       },
       [
         h('div', {}, 'count' + this.count),
         h('button', { onClick: this.onClick }, 'click'),
+        h('button', { onClick: this.onChangePropsDemo1 }, 'onChangePropsDemo1'),
+        h('button', { onClick: this.onChangePropsDemo2 }, 'onChangePropsDemo2'),
+        h('button', { onClick: this.onChangePropsDemo3 }, 'onChangePropsDemo3'),
       ]
     )
   }
